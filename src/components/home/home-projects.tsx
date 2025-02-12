661795ad3f4eb64cbfabcd53eca57aa575368814
Diar Begisbayev <diar_beg64@mail.ru>
2025-02-12 10:30:38 +0500

    feat: added new projects

--- src/components/home/home-projects.tsx
+++ src/components/home/home-projects.tsx
@@ -1,52 +1,9 @@
 'use client'
 
+import { projects } from '@/constants/projects-list'
 import { useTranslation } from '@/i18n/i18n-client.config'
 import Image from 'next/image'
 import React from 'react'
-
-interface Project {
-	logo: string
-	alt: string
-	key: string
-}
-
-const projects: Project[] = [
-	{
-		logo: '/img/projects/ktz-logo.svg',
-		alt: 'project.ktz.alt',
-		key: 'ktz',
-	},
-	{
-		logo: '/img/projects/kaztemirtrans-logo.webp',
-		alt: 'project.kaztemirtrans.alt',
-		key: 'kaztemirtrans',
-	},
-	{
-		logo: '/img/projects/ttk-logo.svg',
-		alt: 'project.ttk.alt',
-		key: 'ttk',
-	},
-	{
-		logo: '/img/projects/cik-logo.webp',
-		alt: 'project.cik.alt',
-		key: 'cik',
-	},
-	{
-		logo: '/img/projects/airastana-logo.png',
-		alt: 'project.airastana.alt',
-		key: 'airastana',
-	},
-	{
-		logo: '/img/projects/astanatower-logo.png',
-		alt: 'project.astanatower.alt',
-		key: 'astanatower',
-	},
-	{
-		logo: '/img/projects/marriott-logo.png',
-		alt: 'project.marriott.alt',
-		key: 'marriott',
-	},
-]
 
 export const HomeProjects: React.FC = () => {
 	const { t } = useTranslation()
@@ -55,8 +12,8 @@ export const HomeProjects: React.FC = () => {
 		<section id='projects' className='bg-gray-50 dark:bg-gray-800'>
 			<div className='mx-auto px-4 lg:px-6 py-8 lg:py-16 max-w-screen-xl'>
 				{/* Header Section */}
-				<div className='mb-6 max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400'>
-					<h2 className='mb-5 font-bold text-4xl text-gray-900 dark:text-white'>
+				<div className='mb-6 max-w-screen-lg text-gray-500 dark:text-gray-400 sm:text-lg'>
+					<h2 className='mb-5 font-bold text-gray-900 dark:text-white text-4xl'>
 						{t('projects.title')}
 					</h2>
 					<p className='font-light'>{t('projects.subtitle')}</p>
@@ -72,16 +29,16 @@ export const HomeProjects: React.FC = () => {
 							<div className='relative flex justify-center items-center mb-6 w-28 h-28'>
 								<Image
 									src={project.logo}
-									alt={t(project.alt)}
-									width={112}
-									height={112}
-									className='w-28 h-28 object-contain'
+									alt={t('projects.title')}
+									width={100}
+									height={100}
+									className='rounded-xl w-28 h-28 object-contain'
 								/>
 							</div>
-							<h3 className='mb-2 font-bold text-gray-900 text-xl dark:text-white'>
+							<h3 className='mb-2 font-bold text-gray-900 dark:text-white text-lg'>
 								{t(`projects.items.${project.key}.name`)}
 							</h3>
-							<p className='text-gray-500 dark:text-gray-300'>
+							<p className='text-gray-500 dark:text-gray-300 text-sm'>
 								{t(`projects.items.${project.key}.description`)}
 							</p>
 						</div>
--- src/components/home/home-services.tsx
+++ src/components/home/home-services.tsx
@@ -33,23 +33,23 @@ export const HomeServices: React.FC = () => {
 		<section id='services' className='bg-gray-50 dark:bg-gray-800'>
 			<div className='mx-auto px-4 lg:px-6 py-8 sm:py-16 max-w-screen-xl'>
 				<div className='mb-8 lg:mb-16 max-w-screen-md'>
-					<h2 className='mb-4 font-extrabold text-4xl text-gray-900 dark:text-white'>
+					<h2 className='mb-4 font-extrabold text-gray-900 dark:text-white text-4xl'>
 						{t('services.title')}
 					</h2>
-					<p className='!text-lg text-gray-500 sm:text-xl dark:text-gray-400'>
+					<p className='text-gray-500 dark:text-gray-400 !text-lg sm:text-xl'>
 						{t('services.subtitle')}
 					</p>
 				</div>
 				<div className='md:gap-12 space-y-8 md:space-y-0 md:grid md:grid-cols-2 lg:grid-cols-3'>
 					{services.map((service, index) => (
 						<div key={index}>
-							<div className='flex justify-center items-center bg-primary/10 dark:bg-primary-900 mb-4 rounded-full w-10 lg:w-12 h-10 lg:h-12'>
+							<div className='flex justify-center items-center bg-primary/10 dark:bg-primary-900 mb-5 rounded-full w-10 lg:w-12 h-10 lg:h-12'>
 								<service.icon className='w-5 lg:w-6 h-5 lg:h-6 text-primary dark:text-primary' />
 							</div>
-							<h3 className='mb-2 font-bold text-xl dark:text-white'>
+							<h3 className='mb-3 font-bold dark:text-white text-xl'>
 								{t(`services.items.${service.key}.title`)}
 							</h3>
-							<p className='text-gray-500 dark:text-gray-400'>
+							<p className='text-gray-500 dark:text-gray-400 text-sm'>
 								{t(`services.items.${service.key}.description`)}
 							</p>
 						</div>
