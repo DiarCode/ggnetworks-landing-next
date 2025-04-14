58952f9e27aa84ba5dfafd372222cd77e97f49d6
DiarCode <diar_beg64@mail.ru>
2025-04-14 13:06:00 +0500

    feat: removed unnseracy namings and descriptions

--- src/components/home/home-footer.tsx
+++ src/components/home/home-footer.tsx
@@ -1,107 +1,107 @@
-'use client'
+"use client";
 
-import { useTranslation } from '@/i18n/i18n-client.config'
-import Link from 'next/link'
-import React from 'react'
+import { useTranslation } from "@/i18n/i18n-client.config";
+import Link from "next/link";
+import React from "react";
 
 const contact = {
-	phone: '+7-7172-97-86-30',
-	email: 'info@ggnetworks.kz',
-}
+  phone: "+7-7172-97-86-30",
+  email: "info@ggnetworks.kz",
+};
 
 const socialLinks = [
-	{
-		platformKey: 'footer.social.instagram',
-		href: 'https://www.instagram.com/ggnetworks_llp/',
-	},
-]
+  {
+    platformKey: "footer.social.instagram",
+    href: "https://www.instagram.com/ggnetworks_llp/",
+  },
+];
 
 const legal = [
-	{ labelKey: 'footer.legal.privacyPolicy', href: '/terms/privacy' },
-	{ labelKey: 'footer.legal.servicePolicy', href: '/terms/service' },
-]
+  { labelKey: "footer.legal.privacyPolicy", href: "/terms/privacy" },
+  { labelKey: "footer.legal.servicePolicy", href: "/terms/service" },
+];
 
 export const HomeFooter: React.FC = () => {
-	const { t } = useTranslation()
-	const currentYear = new Date().getFullYear()
+  const { t } = useTranslation();
+  const currentYear = new Date().getFullYear();
 
-	return (
-		<footer className='bg-gray-50 dark:bg-gray-800 p-4 sm:p-6'>
-			<div className='mx-auto max-w-screen-xl'>
-				<div className='flex justify-between'>
-					{/* Brand and Contact Section */}
-					<div className='mb-6 md:mb-0'>
-						<Link href='/' className='flex items-center'>
-							<span className='font-semibold dark:text-white text-2xl whitespace-nowrap'>
-								GGNetworks
-							</span>
-						</Link>
-						<div className='flex flex-col gap-1 mt-3'>
-							<a
-								className='text-gray-600 dark:text-gray-400 hover:underline'
-								href={`tel:${contact.phone}`}
-							>
-								{contact.phone}
-							</a>
-							<a
-								className='text-gray-600 dark:text-gray-400 hover:underline'
-								href={`mailto:${contact.email}`}
-							>
-								{contact.email}
-							</a>
-						</div>
-					</div>
+  return (
+    <footer className="bg-gray-50 dark:bg-gray-800 p-4 sm:p-6">
+      <div className="mx-auto max-w-screen-xl">
+        <div className="flex justify-between">
+          {/* Brand and Contact Section */}
+          <div className="mb-6 md:mb-0">
+            <Link href="/" className="flex items-center">
+              <span className="font-semibold dark:text-white text-2xl whitespace-nowrap">
+                GGnetworks
+              </span>
+            </Link>
+            <div className="flex flex-col gap-1 mt-3">
+              <a
+                className="text-gray-600 dark:text-gray-400 hover:underline"
+                href={`tel:${contact.phone}`}
+              >
+                {contact.phone}
+              </a>
+              <a
+                className="text-gray-600 dark:text-gray-400 hover:underline"
+                href={`mailto:${contact.email}`}
+              >
+                {contact.email}
+              </a>
+            </div>
+          </div>
 
-					{/* Links Section */}
-					<div className='gap-8 sm:gap-6 grid grid-cols-2'>
-						{/* Follow Us */}
-						<div>
-							<h2 className='mb-6 font-semibold text-gray-900 dark:text-white text-sm uppercase'>
-								{t('footer.followUs')}
-							</h2>
-							<ul className='text-gray-600 dark:text-gray-400'>
-								{socialLinks.map(({ platformKey, href }, index) => (
-									<li key={index} className='mb-4'>
-										<a
-											href={href}
-											target='_blank'
-											rel='noopener noreferrer'
-											className='flex items-center space-x-2 hover:underline'
-										>
-											<span>{t(platformKey)}</span>
-										</a>
-									</li>
-								))}
-							</ul>
-						</div>
+          {/* Links Section */}
+          <div className="gap-8 sm:gap-6 grid grid-cols-2">
+            {/* Follow Us */}
+            <div>
+              <h2 className="mb-6 font-semibold text-gray-900 dark:text-white text-sm uppercase">
+                {t("footer.followUs")}
+              </h2>
+              <ul className="text-gray-600 dark:text-gray-400">
+                {socialLinks.map(({ platformKey, href }, index) => (
+                  <li key={index} className="mb-4">
+                    <a
+                      href={href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex items-center space-x-2 hover:underline"
+                    >
+                      <span>{t(platformKey)}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-						{/* Legal Info */}
-						<div>
-							<h2 className='mb-6 font-semibold text-gray-900 dark:text-white text-sm uppercase'>
-								{t('footer.legal.title')}
-							</h2>
-							<ul className='text-gray-600 dark:text-gray-400'>
-								{legal.map(({ labelKey, href }, index) => (
-									<li key={index} className='mb-4'>
-										<a href={href} className='hover:underline'>
-											{t(labelKey)}
-										</a>
-									</li>
-								))}
-							</ul>
-						</div>
-					</div>
-				</div>
+            {/* Legal Info */}
+            <div>
+              <h2 className="mb-6 font-semibold text-gray-900 dark:text-white text-sm uppercase">
+                {t("footer.legal.title")}
+              </h2>
+              <ul className="text-gray-600 dark:text-gray-400">
+                {legal.map(({ labelKey, href }, index) => (
+                  <li key={index} className="mb-4">
+                    <a href={href} className="hover:underline">
+                      {t(labelKey)}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
 
-				<hr className='sm:mx-auto my-6 lg:my-8 border-gray-200 dark:border-gray-700' />
+        <hr className="sm:mx-auto my-6 lg:my-8 border-gray-200 dark:border-gray-700" />
 
-				{/* Footer Bottom Section */}
-				<div className='sm:flex sm:justify-start sm:items-center'>
-					<span className='text-gray-500 dark:text-gray-400 text-sm sm:text-center'>
-						© {currentYear} GGNetworks. {t('footer.rightsReserved')}
-					</span>
-				</div>
-			</div>
-		</footer>
-	)
-}
+        {/* Footer Bottom Section */}
+        <div className="sm:flex sm:justify-start sm:items-center">
+          <span className="text-gray-500 dark:text-gray-400 text-sm sm:text-center">
+            © {currentYear} GGnetworks. {t("footer.rightsReserved")}
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/components/policy/policy-header.tsx
+++ src/components/policy/policy-header.tsx
@@ -1,116 +1,116 @@
-'use client'
+"use client";
 
-import { useTranslation } from '@/i18n/i18n-client.config'
-import Link from 'next/link'
-import { useState } from 'react'
+import { useTranslation } from "@/i18n/i18n-client.config";
+import Link from "next/link";
+import { useState } from "react";
 
 const menuItems = [
-	{ key: 'home', href: '/#main' },
-	{ key: 'services', href: '/#services' },
-	{ key: 'certificates', href: '/#certificates' },
-	{ key: 'projects', href: '/#projects' },
-	{ key: 'contacts', href: '/#location' },
-]
+  { key: "home", href: "/#main" },
+  { key: "services", href: "/#services" },
+  { key: "certificates", href: "/#certificates" },
+  { key: "projects", href: "/#projects" },
+  { key: "contacts", href: "/#location" },
+];
 
 export const PolicyHeader = () => {
-	const { t } = useTranslation()
-	const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const { t } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	const toggleMenu = () => {
-		setIsMenuOpen(prev => !prev)
-	}
+  const toggleMenu = () => {
+    setIsMenuOpen(prev => !prev);
+  };
 
-	return (
-		<header className='top-0 z-10 sticky sm:py-2'>
-			<nav className='bg-white bg-opacity-50 backdrop-blur-sm mx-auto px-4 sm:px-6 py-4 rounded-[32px] max-w-screen-xl'>
-				<div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
-					{/* Brand */}
-					<Link href='/' className='flex items-center' aria-label='GGNetworks'>
-						<span className='self-center font-semibold text-xl whitespace-nowrap'>
-							GGNetworks
-						</span>
-					</Link>
+  return (
+    <header className="top-0 z-10 sticky sm:py-2">
+      <nav className="bg-white bg-opacity-50 backdrop-blur-sm mx-auto px-4 sm:px-6 py-4 rounded-[32px] max-w-screen-xl">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          {/* Brand */}
+          <Link href="/" className="flex items-center" aria-label="GGnetworks">
+            <span className="self-center font-semibold text-xl whitespace-nowrap">
+              GGnetworks
+            </span>
+          </Link>
 
-					<ul className='hidden lg:flex space-x-6 font-medium'>
-						{menuItems.map(item => (
-							<li key={item.key}>
-								<Link
-									href={item.href}
-									className='text-gray-700 hover:text-primary dark:hover:text-white dark:text-gray-400'
-								>
-									{t(`menu.${item.key}`)}
-								</Link>
-							</li>
-						))}
-					</ul>
+          <ul className="hidden lg:flex space-x-6 font-medium">
+            {menuItems.map(item => (
+              <li key={item.key}>
+                <Link
+                  href={item.href}
+                  className="text-gray-700 hover:text-primary dark:hover:text-white dark:text-gray-400"
+                >
+                  {t(`menu.${item.key}`)}
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-					{/* Phone and Locale Switcher */}
-					<div className='hidden lg:flex items-center space-x-4'>
-						<a
-							href='tel:+7 7172 97 86 30'
-							className='font-medium text-gray-800 dark:text-white hover:underline'
-						>
-							+7 7172 97 86 30
-						</a>
-					</div>
+          {/* Phone and Locale Switcher */}
+          <div className="hidden lg:flex items-center space-x-4">
+            <a
+              href="tel:+7 7172 97 86 30"
+              className="font-medium text-gray-800 dark:text-white hover:underline"
+            >
+              +7 7172 97 86 30
+            </a>
+          </div>
 
-					{/* Mobile Menu Toggle Button */}
-					<button
-						type='button'
-						className='lg:hidden inline-flex items-center hover:bg-gray-100 dark:hover:bg-gray-700 ml-2 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-600 text-gray-500 dark:text-gray-400 text-sm'
-						onClick={toggleMenu}
-						aria-controls='mobile-menu'
-						aria-expanded={isMenuOpen}
-						aria-label={t('menu.toggle')}
-					>
-						<svg
-							className='w-6 h-6'
-							fill='currentColor'
-							viewBox='0 0 20 20'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								fillRule='evenodd'
-								d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-								clipRule='evenodd'
-							/>
-						</svg>
-					</button>
-				</div>
+          {/* Mobile Menu Toggle Button */}
+          <button
+            type="button"
+            className="lg:hidden inline-flex items-center hover:bg-gray-100 dark:hover:bg-gray-700 ml-2 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-600 text-gray-500 dark:text-gray-400 text-sm"
+            onClick={toggleMenu}
+            aria-controls="mobile-menu"
+            aria-expanded={isMenuOpen}
+            aria-label={t("menu.toggle")}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
 
-				{/* Mobile Menu */}
-				{isMenuOpen && (
-					<div
-						id='mobile-menu'
-						className='lg:hidden flex flex-col items-center space-y-4 mt-4'
-					>
-						{/* Mobile Navigation Menu */}
-						<ul className='flex flex-col items-center space-y-4'>
-							{menuItems.map(item => (
-								<li key={item.key}>
-									<Link
-										href={item.href}
-										className='block hover:bg-gray-50 dark:hover:bg-gray-700 px-4 py-2 dark:border-gray-700 text-gray-900 hover:text-primary dark:hover:text-white dark:text-gray-400'
-										onClick={() => setIsMenuOpen(false)} // Close menu on click
-									>
-										{t(`menu.${item.key}`)}
-									</Link>
-								</li>
-							))}
-						</ul>
+        {/* Mobile Menu */}
+        {isMenuOpen && (
+          <div
+            id="mobile-menu"
+            className="lg:hidden flex flex-col items-center space-y-4 mt-4"
+          >
+            {/* Mobile Navigation Menu */}
+            <ul className="flex flex-col items-center space-y-4">
+              {menuItems.map(item => (
+                <li key={item.key}>
+                  <Link
+                    href={item.href}
+                    className="block hover:bg-gray-50 dark:hover:bg-gray-700 px-4 py-2 dark:border-gray-700 text-gray-900 hover:text-primary dark:hover:text-white dark:text-gray-400"
+                    onClick={() => setIsMenuOpen(false)} // Close menu on click
+                  >
+                    {t(`menu.${item.key}`)}
+                  </Link>
+                </li>
+              ))}
+            </ul>
 
-						{/* Mobile Phone and Locale Switcher */}
-						<div className='flex flex-col items-center space-y-2 p-2 border-b rounded-xl w-full'>
-							<a
-								href='tel:+7 7172 97 86 30'
-								className='font-medium text-gray-800 dark:text-white hover:underline'
-							>
-								+7 7172 97 86 30
-							</a>
-						</div>
-					</div>
-				)}
-			</nav>
-		</header>
-	)
-}
+            {/* Mobile Phone and Locale Switcher */}
+            <div className="flex flex-col items-center space-y-2 p-2 border-b rounded-xl w-full">
+              <a
+                href="tel:+7 7172 97 86 30"
+                className="font-medium text-gray-800 dark:text-white hover:underline"
+              >
+                +7 7172 97 86 30
+              </a>
+            </div>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
+};
